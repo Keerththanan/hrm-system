6a0c9fe8908657cc6b9f56118831a187740b23ec
unknown <kerthy92@yahoo.com>
2018-11-19 04:44:46 +0530

    Initial functionality test1

--- src/main/java/com/sgic/hrm/entity/Project.java
+++ src/main/java/com/sgic/hrm/entity/Project.java
@@ -11,5 +11,18 @@ public class Project {
 	Integer id;
 	String project;
 	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	
 
 }
