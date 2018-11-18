678e83597fc47cfb408d9cd65a63d7f15a691bf6
Dilshanth <dilshanth10@gmail.com>
2018-11-19 04:47:08 +0530

    initial functionality test2

--- src/main/java/com/sgic/hrm/entity/ExperienceLetter.java
+++ src/main/java/com/sgic/hrm/entity/ExperienceLetter.java
@@ -7,7 +7,25 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "xpletter", schema = "xpletter")
 public class ExperienceLetter {
-	@Id
-	Integer id;
-	String name;
+  @Id
+  Integer id;
+  String name;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
 }
