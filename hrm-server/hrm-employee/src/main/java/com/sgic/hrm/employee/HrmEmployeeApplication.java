f127d3977c754f67a3b7ecd3f8455869b1343bf0
Keerththanan <kerthy92@gmail.com>
2018-11-20 13:48:29 +0530

    Folder structures changed - Seperated as layers

---
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/HrmEmployeeApplication.java
@@ -0,0 +1,12 @@
+package com.sgic.hrm.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmEmployeeApplication.class, args);
+	}
+}
