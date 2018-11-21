83e2acd0cc394228f68aa47c681cdb292420cd97
Jeyarni <jeya08sivam@gmail.com>
2018-11-21 17:18:21 +0530

    RequestPromotion

---
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/entity/RequestPromotion.java
@@ -0,0 +1,79 @@
+package com.sgic.hrm.commons.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+@Entity
+@Table(schema = "employee", name = "request_promotion")
+public class RequestPromotion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private int userId;
+	
+	@JoinColumn(name = "id")
+//	private Designation designationId;
+	private String promotionRemark;
+	private String recommendedBy;
+	
+	@CreationTimestamp
+	private Date createdAt;
+
+//	public Designation getDesignationId() {
+//		return designationId;
+//	}
+//
+//	public void setDesignationId(Designation designationId) {
+//		this.designationId = designationId;
+//	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getPromotionRemark() {
+		return promotionRemark;
+	}
+
+	public void setPromotionRemark(String promotionRemark) {
+		this.promotionRemark = promotionRemark;
+	}
+
+	public String getRecommendedBy() {
+		return recommendedBy;
+	}
+
+	public void setRecommendedBy(String recommendedBy) {
+		this.recommendedBy = recommendedBy;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
