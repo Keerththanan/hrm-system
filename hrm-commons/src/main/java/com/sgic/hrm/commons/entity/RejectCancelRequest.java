98d86a8bab7ba448e09ad2fb9fc94f213ed6dd94
anushanthR <anushanu@live.com>
2018-11-21 12:23:46 +0530

    leave system pom updated

--- hrm-commons/src/main/java/com/sgic/hrm/commons/entity/RejectCancelRequest.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/entity/RejectCancelRequest.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "reject_leave_request")
+@Table(name = "reject_cancel_request")
 public class RejectCancelRequest implements Serializable {
 
   /**
