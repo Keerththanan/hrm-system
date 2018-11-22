299f57a788eecd42ff5d3ee4cb33c1786fc98e35
Jeyarni <jeya08sivam@gmail.com>
2018-11-22 11:35:04 +0530

    promotion

--- hrm-commons/src/main/java/com/sgic/hrm/commons/repository/RequestPromotionRepository.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/repository/RequestPromotionRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sgic.hrm.commons.entity.RequestPromotion;
 
-public interface RequestPrommotionRepository extends JpaRepository<RequestPromotion, Integer>{
+public interface RequestPromotionRepository extends JpaRepository<RequestPromotion, Integer>{
 
 }
--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/RequestPromotionController.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/RequestPromotionController.java
@@ -13,47 +13,48 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sgic.hrm.commons.entity.RequestPromotion;
 import com.sgic.hrm.employee.service.RequestPromotionService;
 
 @RestController
 public class RequestPromotionController {
-//	@Autowired
-//	private RequestPromotionService requestPromotionService;
-//
-//	@PostMapping("/promotion")
-//	public HttpStatus requestPromotion(@RequestBody RequestPromotionData requestPromotionData) {
-//		boolean reqPro = requestPromotionService.addRequestPromotion(requestPromotion);
-//		if (reqPro) {
-//			return HttpStatus.CREATED;
-//		}
-//		return HttpStatus.BAD_REQUEST;
-//
-//	}
-//
-//	@GetMapping("/promotion")
-//	public ResponseEntity<List<RequestPromotionData>> getAllRequestPromotions() {
-//		List<RequestPromotionData> reqPromotionData = requestPromotionService.getAllRequestPromotion();
-//		ResponseEntity<List<RequestPromotionData>> response = new ResponseEntity<>(reqPromotionData, HttpStatus.OK);
-//		return response;
-//	}
-//
-//	@PutMapping("/promotion/{id}")
-//	public HttpStatus updateRequestPromotion(@PathVariable Integer id, @RequestBody RequestPromotionData requestPromotionData) {
-//		boolean reqPro = requestPromotionService.updateRequestPromotion(requestPromotion, id);
-//		if (reqPro) {
-//			return HttpStatus.ACCEPTED;
-//		}
-//		return HttpStatus.BAD_REQUEST;
-//
-//	}
-//
-//	@DeleteMapping("/promotion/{id}")
-//	public HttpStatus deleteRequestPromotion(@PathVariable Integer id) {
-//		boolean reqPro = requestPromotionService.deleteRequstPromotion(id);
-//		if (reqPro) {
-//			return HttpStatus.ACCEPTED;
-//		}
-//		return HttpStatus.BAD_REQUEST;
-//	}
+	@Autowired
+	private RequestPromotionService requestPromotionService;
+
+	@PostMapping("/promotion")
+	public HttpStatus requestPromotion(@RequestBody RequestPromotion requestPromotion) {
+		boolean reqPro = requestPromotionService.addRequestPromotion(requestPromotion);
+		if (reqPro) {
+			return HttpStatus.CREATED;
+		}
+		return HttpStatus.BAD_REQUEST;
+
+	}
+
+	@GetMapping("/promotion")
+	public ResponseEntity<List<RequestPromotion>> getAllRequestPromotions() {
+		List<RequestPromotion> reqPromotionData = requestPromotionService.getAllRequestPromotion();
+		ResponseEntity<List<RequestPromotion>> response = new ResponseEntity<>(reqPromotionData, HttpStatus.OK);
+		return response;
+	}
+
+	@PutMapping("/promotion/{id}")
+	public HttpStatus updateRequestPromotion(@PathVariable Integer id, @RequestBody RequestPromotion requestPromotion) {
+		boolean reqPro = requestPromotionService.updateRequestPromotion(requestPromotion, id);
+		if (reqPro) {
+			return HttpStatus.ACCEPTED;
+		}
+		return HttpStatus.BAD_REQUEST;
+
+	}
+
+	@DeleteMapping("/promotion/{id}")
+	public HttpStatus deleteRequestPromotion(@PathVariable Integer id) {
+		boolean reqPro = requestPromotionService.deleteRequstPromotion(id);
+		if (reqPro) {
+			return HttpStatus.ACCEPTED;
+		}
+		return HttpStatus.BAD_REQUEST;
+	}
 
 }
