1d3d6bcaeab98c5cc8bd9eefe96e652061c69744
Sivakaresh <sivakaresh@gmail.com>
2018-11-26 21:59:22 +0530

    crossOrigin error fixed for salary details

--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/remuneration/controller/SalaryDetailsHRViewController.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/remuneration/controller/SalaryDetailsHRViewController.java
@@ -3,6 +3,7 @@ package com.sgic.hrm.employee.remuneration.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.sgic.hrm.commons.dto.SalaryDetailsDto;
 import com.sgic.hrm.commons.entity.mapper.SalaryDetailsEntityMapper;
 import com.sgic.hrm.employee.remuneration.service.SalaryDetailsHRViewService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class SalaryDetailsHRViewController {
 
