224d07065b5fe725697feaa6cacc7295b65a65dd
Mayuran <tmayuran67@gmail.com>
2018-12-03 18:11:12 +0530

    modify loan

--- hrm-commons/src/main/java/com/sgic/hrm/commons/repository/UserLoanDetailsRepository.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/repository/UserLoanDetailsRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+
 import com.sgic.hrm.commons.entity.UserLoanDetails;
 
 public interface UserLoanDetailsRepository extends JpaRepository<UserLoanDetails, Integer> {
 
-	@Query("SELECT ul from UserLoanDetails ul WHERE ul.user.userId = ?1")
-	List<UserLoanDetails> findByUserId(Integer userId);
+	@Query("SELECT ul from UserLoanDetails ul WHERE ul.user.id = ?1")
+	List<UserLoanDetails> findByUserId(Integer id);
 	
 }
