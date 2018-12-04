4f73ae68b520a0dd83fe924787c66f9f295b8bb1
jana <jjananthan93@gmail.com>
2018-12-04 11:02:49 +0530

    appointment type error solve

--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/AppointmentTypeController.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/controller/AppointmentTypeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,7 +53,7 @@ public class AppointmentTypeController {
 		return HttpStatus.BAD_REQUEST;
 	}
 	
-	@PutMapping("/appointmentType/{id}")
+	@DeleteMapping("/appointmentType/{id}")
 	public HttpStatus deleteAppointmentType(@PathVariable Integer id,@RequestBody AppointmentType appointmentType) {
 		boolean deleteTest=appointmentTypeService.deleteAppointmentType(id);
 		if(deleteTest) {
