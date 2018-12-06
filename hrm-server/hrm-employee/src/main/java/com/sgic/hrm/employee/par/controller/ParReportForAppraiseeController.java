82129c4ee531c8867f924ec9b7d1da048a7b1ded
Thiruparan <thirupparan1994@gmail.com>
2018-12-06 17:48:44 +0530

    par report

--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/par/controller/ParReportForAppraiseeController.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/par/controller/ParReportForAppraiseeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import com.sgic.hrm.commons.dto.par.ReportParAppraiseeDtoPost;
 import com.sgic.hrm.commons.dto.par.ScoreParAppraiseeDtoGet;
 import com.sgic.hrm.commons.dto.par.ScoreParAppraiseeDtoPost;
 import com.sgic.hrm.employee.par.serviceCombined.ParReportForAppraiseeService;
-
+@CrossOrigin
 @RestController
 public class ParReportForAppraiseeController {
 	@Autowired
