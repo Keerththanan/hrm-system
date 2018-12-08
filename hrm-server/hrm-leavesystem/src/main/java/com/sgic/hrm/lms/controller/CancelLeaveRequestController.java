ba46d3dcd9fd8b4281309eed5f33c85f208a6da9
bihaSobiha <sobiha09@gmail.com>
2018-12-08 16:37:19 +0530

    cancel leave request

--- hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/controller/CancelLeaveRequestController.java
+++ hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/controller/CancelLeaveRequestController.java
@@ -5,6 +5,7 @@ package com.sgic.hrm.lms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,8 @@ import com.sgic.hrm.lms.service.CancelLeaveRequestService;
  * @author Anushanth
  *
  */
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/cancel")
 public class CancelLeaveRequestController {
