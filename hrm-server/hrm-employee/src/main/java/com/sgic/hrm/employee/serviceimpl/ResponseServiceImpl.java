bf538e739781373a8a81d55628978cfa98301518
sjanahans <sjanahans@gmail.com>
2018-12-09 06:24:11 +0530

    individual response view

--- hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/serviceimpl/ResponseServiceImpl.java
+++ hrm-server/hrm-employee/src/main/java/com/sgic/hrm/employee/serviceimpl/ResponseServiceImpl.java
@@ -3,7 +3,9 @@ package com.sgic.hrm.employee.serviceimpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.sgic.hrm.commons.entity.Response;
+import com.sgic.hrm.commons.repository.LoginRepository;
 import com.sgic.hrm.commons.repository.ResponseRepository;
 import com.sgic.hrm.employee.service.ResponseService;
 
@@ -12,6 +14,10 @@ public class ResponseServiceImpl implements ResponseService {
 
 	@Autowired
 	private ResponseRepository responseRepository;
+	
+	@Autowired
+	private LoginRepository loginRepository;
+	
 	@Override
 	public List<Response> viewResponse() {
 		return responseRepository.findAll();
@@ -21,6 +27,11 @@ public class ResponseServiceImpl implements ResponseService {
 	public boolean addResponse(Response response) {
 		responseRepository.save(response);
 		return true;
+	}
+
+	@Override
+	public List<Response> getSpecificUser(String name) {
+		return responseRepository.findByRelevantIndividual(loginRepository.findByUsername(name).get().getUser());
 	}
 
 }
