45fdb89c2927fc3d7186ae1e96791277a696faa0
kajaluxshi <luxshibala@gmail.com>
2018-12-13 08:38:08 +0530

    Data added

--- hrm-server/hrm-login/src/main/resources/data.sql
+++ hrm-server/hrm-login/src/main/resources/data.sql
@@ -171,3 +171,122 @@ INSERT INTO employee.authorize_type(authorize_type_name) VALUES('Delete');
 --INSERT INTO employee.permission(authorize_type_id, module_id) VALUES(2,2);
 --INSERT INTO employee.permission(authorize_type_id, module_id) VALUES(3,2);
 --INSERT INTO employee.permission(authorize_type_id, module_id) VALUES(4,2);
+
+--
+-- Data for Name: careerdevelopmentplan; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.careerdevelopmentplan VALUES (1, 'Communication skill');
+INSERT INTO employee.careerdevelopmentplan VALUES (2, 'WSO2');
+INSERT INTO employee.careerdevelopmentplan VALUES (3, 'Databse designing');
+INSERT INTO employee.careerdevelopmentplan VALUES (4, 'Project mangement');
+INSERT INTO employee.careerdevelopmentplan VALUES (5, 'QA Automation');
+
+--
+-- Data for Name: user_career_development_plan_company; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.user_career_development_plan_self VALUES (1, 'Planning', 1, 2);
+INSERT INTO employee.user_career_development_plan_self VALUES (2, 'Processing', 2, 2);
+INSERT INTO employee.user_career_development_plan_self VALUES (3, 'Processing', 3, 3);
+INSERT INTO employee.user_career_development_plan_self VALUES (4, 'Planning', 2, 3);
+INSERT INTO employee.user_career_development_plan_self VALUES (5, 'Procesing', 4, 4);
+
+--
+-- Data for Name: user_career_development_plan_self; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.user_career_development_plan_company VALUES (1, 'Planning', 3, 2);
+INSERT INTO employee.user_career_development_plan_company VALUES (2, 'Processing', 4, 2);
+INSERT INTO employee.user_career_development_plan_company VALUES (3, 'Processing', 1, 3);
+INSERT INTO employee.user_career_development_plan_company VALUES (4, 'Planning', 4, 3);
+INSERT INTO employee.user_career_development_plan_company VALUES (5, 'Procesing', 1, 4);
+
+
+--
+-- Data for Name: welfare_event; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.welfare_event VALUES (1, '2018-12-25', 'Approved', 100000, 'Seminar', 1);
+INSERT INTO employee.welfare_event VALUES (3, '2018-12-29', 'Approved', 10000, 'Seminar', 1);
+INSERT INTO employee.welfare_event VALUES (2, '2018-12-17', 'Approved', 200000, 'Sports meet', 1);
+
+
+--
+-- Data for Name: welfare_type; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.welfare_type VALUES (1, 'General welfare');
+INSERT INTO employee.welfare_type VALUES (2, 'Indiviual welfare');
+
+
+
+--
+-- Data for Name: highest_qualification; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.highest_qualification VALUES (1, 'Bsc in IT');
+INSERT INTO employee.highest_qualification VALUES (2, 'Bsc ');
+
+
+--
+-- Data for Name: job; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.job VALUES (1, 'QA', 'QA001');
+INSERT INTO employee.job VALUES (2, 'HR', 'HR002');
+
+
+--
+-- Data for Name: applicant; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.applicant VALUES (1, 'Jaffna', '\xaced00057372000c6a6176612e696f2e46696c65042da4450e0de4ff0300014c0004706174687400124c6a6176612f6c616e672f537472696e673b787074001b433a5c66616b65706174685c64622064657461696c732e646f63787702005c78', '2018-12-05', 'hello@gmail.com', 'Kaji', '384823V', 1, 2);
+INSERT INTO employee.applicant VALUES (9, 'Jaffna', NULL, '2018-12-01', 'yasoselva@gmail.com', 'Yaso', '38945034V', 1, 1);
+
+--
+-- Data for Name: designation; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.designation VALUES (1, 'Associate Software Engineer');
+INSERT INTO employee.designation VALUES (2, 'Software Engineer');
+
+
+
+--
+-- Data for Name: denied_promotion; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.denied_promotion VALUES (1, 1, '2018-12-10', 'par score', 1, 1);
+INSERT INTO employee.denied_promotion VALUES (2, 2, '2018-11-12', 'Leave', 2, 1);
+
+
+--
+-- Data for Name: promotion; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.promotion VALUES (1, '2', '2010-12-12', 'par score', 50000, 1, 1);
+
+
+--
+-- Data for Name: recuitment; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.recuitment VALUES (1, 'Internal');
+INSERT INTO employee.recuitment VALUES (2, 'External');
+
+
+--
+-- Data for Name: request_promotion; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.request_promotion VALUES (1, '2010-12-12', 'Career achivement', '1', '2010-12-12', 1, 1);
+INSERT INTO employee.request_promotion VALUES (2, '2018-12-12', 'Par score', '2', '2018-12-12', 2, 1);
+
+
+--
+-- Data for Name: vacancy; Type: TABLE DATA; Schema: employee; Owner: postgres
+--
+
+INSERT INTO employee.vacancy VALUES (1, '2018-06-07', 'Internal', 3, 10000, '2018-06-07', '2019-03-04', 1, 1, 1, 1);
+INSERT INTO employee.vacancy VALUES (2, '2018-06-07', 'External', 5, 30000, '2018-06-07', '2019-03-04', 1, 1, 1, 1);
