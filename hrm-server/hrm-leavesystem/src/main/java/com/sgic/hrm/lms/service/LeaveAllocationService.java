1c294fa86095461f1269be3bc02dc407d59441e1
anushanthR <anushanu@live.com>
2018-12-19 00:09:05 +0530

    get LeaveType By user and value

--- hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/service/LeaveAllocationService.java
+++ hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/service/LeaveAllocationService.java
@@ -2,6 +2,7 @@ package com.sgic.hrm.lms.service;
 
 import java.util.List;
 import com.sgic.hrm.commons.entity.AppointmentType;
+import com.sgic.hrm.commons.entity.CarryforwardRequest;
 import com.sgic.hrm.commons.entity.LeaveAllocation;
 import com.sgic.hrm.commons.entity.LeaveRequest;
 import com.sgic.hrm.commons.entity.LeaveType;
@@ -20,4 +21,8 @@ public interface LeaveAllocationService {
   void resetLeaveAllocationMonthly();
 
   boolean deleteLeaveAllocation(Integer id);
+  
+  boolean deductCarryforwardedDays(CarryforwardRequest carryforwardRequest);
+  
+  LeaveAllocation getByUserAndType(User user);
 }
