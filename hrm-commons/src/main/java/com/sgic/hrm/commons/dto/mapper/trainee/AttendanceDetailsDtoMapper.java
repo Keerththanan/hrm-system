40c75fcbd24f0963a91af3efa585532ca6f4337b
paki <tpakikaran14@gmail.com>
2019-01-09 16:25:05 +0530

    Attendance modified

--- hrm-commons/src/main/java/com/sgic/hrm/commons/dto/mapper/trainee/AttendanceDetailsDtoMapper.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/dto/mapper/trainee/AttendanceDetailsDtoMapper.java
@@ -7,23 +7,22 @@ import com.sgic.hrm.commons.entity.trainee.AttendanceDetails;
 import com.sgic.hrm.commons.entity.trainee.Trainee;
 
 public class AttendanceDetailsDtoMapper {
-	public static AttendanceDetails map(AttendanceDetailsDto attendanceDetailsDto){
+	public static AttendanceDetails map(AttendanceDetailsDto attendanceDetailsDto) {
 		AttendanceDetails attendanceDetails = new AttendanceDetails();
 		Trainee trainee = new Trainee();
 		AttendType attendType = new AttendType();
 		AttendStatus attendStatus = new AttendStatus();
 
 		attendanceDetails.setId(attendanceDetailsDto.getId());
-		attendanceDetails.setStartDate(attendanceDetailsDto.getStartDate());
-		attendanceDetails.setEndDate(attendanceDetailsDto.getEndDate());
+		attendanceDetails.setStartTime(attendanceDetailsDto.getStartTime());
+		attendanceDetails.setEndTime(attendanceDetailsDto.getEndTime());
+		attendanceDetails.setDuration(attendanceDetailsDto.getDuration());
 		attendanceDetails.setUpdateAt(attendanceDetailsDto.getUpdateAt());
-
+		attendanceDetails.setAttendDate(attendanceDetailsDto.getAttendDate());
 		attendType.setId(attendanceDetailsDto.getAttendType());
 		trainee.setId(attendanceDetailsDto.getTrainee());
 		attendStatus.setId(attendanceDetailsDto.getAttendantStatus());
 		return attendanceDetails;
 	}
-
-	
 
 }
