00a7084bb3d4b3504128993c2de353db199c26ce
sujaany <ssujaany09@gmail.com>
2019-01-12 14:06:25 +0530

    conflict resolved

--- hrm-commons/src/main/java/com/sgic/hrm/commons/trainer/repository/TrainerRepository.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/trainer/repository/TrainerRepository.java
@@ -1,16 +1,9 @@
 package com.sgic.hrm.commons.trainer.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.sgic.hrm.commons.entity.trainer.Trainer;
 
-<<<<<<< HEAD
-public interface TrainerRepository extends JpaRepository<Trainer, Integer> {
-
-=======
 public interface TrainerRepository extends JpaRepository<Trainer, Integer>{
-	Trainer findTrainerById(Integer id);
+//	Trainer findTrainerById(Integer id);
 //	Trainer findTrainerByName(String username);
->>>>>>> 25335ffa3b3f8812eadcea9a71ac58ced17d3f54
 }
