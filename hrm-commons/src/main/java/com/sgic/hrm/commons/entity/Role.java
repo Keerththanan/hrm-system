71d117e2455ba6bf27aa90b84081e27407ee347c
ragulan22 <balasingamragulan@gmail.com>
2019-01-13 16:50:24 +0530

    feedback fieald add

--- hrm-commons/src/main/java/com/sgic/hrm/commons/entity/Role.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/entity/Role.java
@@ -1,7 +1,6 @@
 package com.sgic.hrm.commons.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,50 +9,48 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.NaturalId;
-
 import com.sgic.hrm.commons.enums.RoleName;
 
 @Entity
-@Table(name ="role", schema = "login")
+@Table(name = "role", schema = "login")
 public class Role implements Serializable {
-	/**
-		 * 
-		 */
-	private static final long serialVersionUID = -4287459510756933063L;
+  /**
+  	 * 
+  	 */
+  private static final long serialVersionUID = -4287459510756933063L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	@Enumerated(EnumType.STRING)
-	@NaturalId
-	@Column(name="rolename",length = 60)
-	private RoleName name;
+  @Enumerated(EnumType.STRING)
+  @NaturalId
+  @Column(name = "rolename", length = 60)
+  private RoleName name;
 
-	public Role() {
+  public Role() {
 
-	}
-	
-	public Role(RoleName name) {
-		this.name = name;
-	}
+  }
 
-	public Long getId() {
-		return id;
-	}
+  public Role(RoleName name) {
+    this.name = name;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public RoleName getName() {
-		return name;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public void setName(RoleName name) {
-		this.name = name;
-	}
+  public RoleName getName() {
+    return name;
+  }
+
+  public void setName(RoleName name) {
+    this.name = name;
+  }
 
 }
--- hrm-commons/src/main/java/com/sgic/hrm/commons/trainer/repository/TrainerRepository.java
+++ hrm-commons/src/main/java/com/sgic/hrm/commons/trainer/repository/TrainerRepository.java
@@ -3,17 +3,17 @@ package com.sgic.hrm.commons.trainer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sgic.hrm.commons.entity.trainer.Trainer;
 
-public interface TrainerRepository extends JpaRepository<Trainer, Integer>{
-//	Trainer findTrainerById(Integer id);
-//	Trainer findTrainerByName(String fullname);
-	
-//	@Query("select p from Payment p join p.trainingSchedule t where t.trainer_id=?1")
-//	List<Payment> findByTrainerId(int id);
+public interface TrainerRepository extends JpaRepository<Trainer, Integer> {
+  // Trainer findTrainerById(Integer id);
+  // Trainer findTrainerByName(String fullname);
 
-	
-//	select * 
-//	from trainer.payment join trainer.training_schedule 
-//	on trainer.payment.training_schedule_id=trainer.training_schedule.id
-//	where trainer.training_schedule.date <= NOW() AND trainer.training_schedule.trainer_id=2;
+  // @Query("select p from Payment p join p.trainingSchedule t where t.trainer_id=?1")
+  // List<Payment> findByTrainerId(int id);
+
+
+  // select *
+  // from trainer.payment join trainer.training_schedule
+  // on trainer.payment.training_schedule_id=trainer.training_schedule.id
+  // where trainer.training_schedule.date <= NOW() AND trainer.training_schedule.trainer_id=2;
 
 }
