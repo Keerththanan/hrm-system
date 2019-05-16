159ae27277dc15861ffc8f510d66d7c60a77e297
jananthan93 <jjananthan93@gmail.com>
2019-05-17 05:25:56 +0530

    deployment dependency modification

--- hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/HrmLeaveManagementSystemApplication.java
+++ hrm-server/hrm-leavesystem/src/main/java/com/sgic/hrm/lms/HrmLeaveManagementSystemApplication.java
@@ -3,6 +3,7 @@ package com.sgic.hrm.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EntityScan(basePackages = {"com.sgic.hrm.commons.entity"})
 @EnableJpaRepositories(basePackages = {"com.sgic.hrm.commons.repository"})
-public class HrmLeaveManagementSystemApplication {
+public class HrmLeaveManagementSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmLeaveManagementSystemApplication.class, args);
